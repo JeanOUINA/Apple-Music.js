bc587478a33b3a4009d1f4e4a8bffcaf0ec4640a
Not Thomiz <jeanouina@gmail.com>
2023-07-01 13:39:31 +0200

    my bad

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import { getMetadata } from "./metadata";
-
 export * from "./metadata";
 export * from "./controls";
 export * from "./osascript";
-
-getMetadata()
-.then(metadata => {
-    console.log(metadata);
-})
